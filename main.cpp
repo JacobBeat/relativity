116a0bfc4f8893ac0b61b8e006e0d5e01eeba0b9
Jacob Beatty <jacobbeatty1023@gmail.com>
2026-08-13 13:11:53 -0400

    glew + opengl set up

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <GL/glew.h>
+
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 #include <array>
@@ -6,38 +8,13 @@
 constexpr int screen_width = 800;
 constexpr int screen_height = 600;
 
-struct Surface {
-  std::vector<double> xyzArray;
-
-  Surface(int VertexCountPerSide) {
-    const int CoordinateCount = VertexCountPerSide * VertexCountPerSide * 3;
-    const double vertexStep = 2.0 / (VertexCountPerSide - 1);
-
-    xyzArray.resize(VertexCountPerSide);
-
-    for (int row = 0; row < VertexCountPerSide; row++) {
-      for (int column = 0; column < VertexCountPerSide; column++) {
-        const double x = -1.0 + column * vertexStep;
-        const double y = -1.0 + row * vertexStep;
-        const double z = 0.0;
-
-        const double vertexStartIndex = (row * VertexCountPerSide + column) * 3;
-        xyzArray[vertexStartIndex] = x;
-        xyzArray[vertexStartIndex + 1] = y;
-        xyzArray[vertexStartIndex + 2] = z;
-      }
-    }
-  }
-};
-
 int main() {
   if (!glfwInit()) {
     return -1;
   }
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
   GLFWwindow *window =
       glfwCreateWindow(screen_width, screen_height, "3D Window", NULL, NULL);
 
@@ -48,9 +25,11 @@ int main() {
 
   glfwMakeContextCurrent(window);
   glfwSwapInterval(1);
+  glewInit();
+
+  glViewport(0, 0, screen_width, screen_height);
 
   while (!glfwWindowShouldClose(window)) {
-    glClear(GL_COLOR_CLEAR_VALUE);
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
