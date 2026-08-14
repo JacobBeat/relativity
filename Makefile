884287acda998bad05a2c178e66322e0ca61a307
Jacob Beatty <jacobbeatty1023@gmail.com>
2026-08-14 15:16:55 -0400

    opengl code

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 main: main.o 
-	g++ -lglfw -lGL main.o -o main 
+	g++ -lGLEW -lglfw -lGL main.o -o main 
 
 main.o: main.cpp  
 	g++ -c main.cpp  
--- main.cpp
+++ main.cpp
@@ -3,17 +3,83 @@
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 #include <array>
+#include <iostream>
 #include <vector>
 
 constexpr int screen_width = 800;
 constexpr int screen_height = 600;
 
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout(location = 0) in vec3 vertexPosition_modelspace;\n"
+    "void main() {\n"
+    "gl_Position.xyz = vertexPosition_modelspace;\n"
+    "gl_Position.w = 1.0;\n"
+    "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;"
+                                   "void main() {\n"
+                                   "FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
+                                   "}\0";
+
+void checkShaderCompile(GLuint shader, const char *label) {
+  int success;
+  char infoLog[512];
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(shader, 512, NULL, infoLog);
+    std::cerr << "Shader compile error (" << label << "): " << infoLog
+              << std::endl;
+  }
+}
+
+void checkProgramLink(GLuint program) {
+  int success;
+  char infoLog[512];
+  glGetProgramiv(program, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(program, 512, NULL, infoLog);
+    std::cerr << "Program link error: " << infoLog << std::endl;
+  }
+}
+
+GLuint createShaders() {
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+  checkShaderCompile(vertexShader, "vertex");
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+  checkShaderCompile(fragmentShader, "fragment");
+
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  checkProgramLink(shaderProgram);
+  glUseProgram(shaderProgram);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+  return shaderProgram;
+}
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
 int main() {
   if (!glfwInit()) {
     return -1;
   }
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   GLFWwindow *window =
       glfwCreateWindow(screen_width, screen_height, "3D Window", NULL, NULL);
@@ -29,7 +95,31 @@ int main() {
 
   glViewport(0, 0, screen_width, screen_height);
 
+  float vertex_data[] = {-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0};
+
+  unsigned int vertex_array_object;
+  glGenVertexArrays(1, &vertex_array_object);
+  glBindVertexArray(vertex_array_object);
+
+  unsigned int vertex_buffer_object;
+  glGenBuffers(1, &vertex_buffer_object);
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
+
+  glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), vertex_data, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  GLuint shaderProgram = createShaders();
+
   while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(vertex_array_object);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
